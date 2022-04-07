b095790643f5ff787522de71966c479c8a0d249a
daduckster <vlasak.o@yandex.ru>
2022-04-07 14:36:10 +0200

    first commit

---
+++ src/components/YourNotes/YourNotes.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from './YourNotes.module.scss'
+
+function YourNotes() {
+  return (
+    <div>
+      YourNotes
+    </div>
+  );
+}
+
+export default YourNotes;
